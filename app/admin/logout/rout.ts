2f5f8fdea3176b6856f92b42082cef4e6c8ff5b0
J3N3R4L <66448107+J3N3R4L@users.noreply.github.com>
2026-02-03 01:40:31 +0200

    Update rout.ts

--- app/admin/logout/rout.ts
+++ app/admin/logout/rout.ts
@@ -1,1 +1,11 @@
+// app/admin/logout/route.ts
+import { NextResponse } from "next/server";
 
+export async function POST(req: Request) {
+  const res = NextResponse.redirect(new URL("/admin/login", req.url), 303);
+
+  res.cookies.set("err_user_id", "", { path: "/", maxAge: 0 });
+  res.cookies.set("err_role", "", { path: "/", maxAge: 0 });
+
+  return res;
+}
